17cf507365952ae9d06a1f462b1de43fadb1b08d
Nail <nailtagiyev1999@gmail.com>
2023-03-01 15:30:19 +0400

    create 2 microservices

---
+++ employee-service/src/main/java/com/spring/microservices/EmployeeServiceApplication.java
@@ -0,0 +1,11 @@
+package com.spring.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
+}
