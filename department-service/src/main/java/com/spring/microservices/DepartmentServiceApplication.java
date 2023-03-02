e801a602073d77a4d30fae10d1d7f8f7ffc0347d
Nail <nailtagiyev1999@gmail.com>
2023-03-02 15:36:44 +0400

    add @RefreshScope

--- department-service/src/main/java/com/spring/microservices/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/spring/microservices/DepartmentServiceApplication.java
@@ -2,10 +2,12 @@ package com.spring.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
 public class DepartmentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DepartmentServiceApplication.class, args);
