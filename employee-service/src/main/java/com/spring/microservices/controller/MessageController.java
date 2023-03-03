d053a39950b77a4a52c1ec4616a4c87532c1d8ba
Nail <nailtagiyev1999@gmail.com>
2023-03-03 11:15:38 +0400

    add Spring Cloud Bus

---
+++ employee-service/src/main/java/com/spring/microservices/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.spring.microservices.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Value("${spring.boot.message}")
+    private String message;
+
+    @GetMapping("/users/message")
+    public String getMessage() {
+        return message;
+    }
+}
